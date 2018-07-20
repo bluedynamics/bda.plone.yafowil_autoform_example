737efd6ec5ab4987ef1b930f3eb430fec5b5f95e
Robert Niederreiter <office@squarewave.at>
2018-07-20 09:19:25 +0200

    Field with factory_callable in IYafowilAutoformExampleBehavior

--- src/bda/plone/yafowil_autoform_example/behaviors.py
+++ src/bda/plone/yafowil_autoform_example/behaviors.py
@@ -1,6 +1,7 @@
 from plone.autoform.interfaces import IFormFieldProvider
 from plone.supermodel import model
 from yafowil.base import UNSET
+from yafowil.base import factory
 from yafowil.plone.autoform import directives
 from zope import schema
 from zope.i18nmessageid import MessageFactory
@@ -14,7 +15,7 @@ def autoform_field_1_value(context, widget, data):
     """Getter function for ``IYafowilAutoformExampleBehavior.autoform_field_1``
     value.
     """
-    return getattr(context, 'autoform_field_1', UNSET)
+    return context.autoform_field_1
 
 
 def autoform_field_1_vocab(context, widget, data):
@@ -26,6 +27,34 @@ def autoform_field_1_vocab(context, widget, data):
         ('opt_2', _('opt_2', default=u'Option 2')),
         ('opt_3', _('opt_3', default=u'Option 3'))
     ]
+
+
+def autoform_field_2_factory(context):
+    """Factory callback for ``IYafowilAutoformExampleBehavior.autoform_field_2``.
+    """
+    value = context.autoform_field_2
+    value = value if value else []
+    array = factory('#field:#array',
+        name='autoform_field_2',
+        value=value,
+        props={
+            'label': _(u'autoform_field_2', default=u'Autoform Field 2'),
+            'array.label': ' ',
+            'help': _(u'autoform_field_2_description',
+                      default=u'Autoform Field 2 Description'),
+            'required': _(u'autoform_field_2_required',
+                          default=u'Autoform Field 2 must at least contain one entry'),
+            'persist': True
+        })
+    array['field'] = factory(
+        '#arrayfield:text',
+        props={
+            'label': _(u'autoform_field_2_entry', default=u'Entry'),
+            'help': _(u'autoform_field_2_entry_help', default=u'Array Entry'),
+            'required': _(u'autoform_field_2_entry_required',
+                          default=u'Array Entry cannot be empty'),
+        })
+    return array
 
 
 @provider(IFormFieldProvider)
@@ -47,4 +76,15 @@ class IYafowilAutoformExampleBehavior(model.Schema):
                       default=u'Autoform Field 1 Description'),
             'vocabulary': autoform_field_1_vocab
         }
+    )
+
+    autoform_field_2 = schema.Tuple(required=False)
+    directives.order(
+        'autoform_field_2',
+        fieldset='default',
+        after='autoform_field_1'
+    )
+    directives.factory_callable(
+        'autoform_field_2',
+        autoform_field_2_factory
     )
