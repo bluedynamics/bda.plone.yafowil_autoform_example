4eb6d780e3bddc99703f52439dbf8f2fb8bb96bc
Robert Niederreiter <office@squarewave.at>
2019-03-16 08:42:59 +0100

    yafowil example view WIP

--- src/bda/plone/yafowil_autoform_example/browser/__init__.py
+++ src/bda/plone/yafowil_autoform_example/browser/__init__.py
@@ -1,1 +1,8 @@
-#
+from Products.Five import BrowserView
+
+
+class YafowilExampleView(BrowserView):
+
+    def __init__(self, context, request):
+        super(YafowilExampleView, self).__init__(context, request)
+        print(80 * '#')
