00836557533911cc2bbffc919d2ec66d63f653dc
Robert Niederreiter <office@squarewave.at>
2019-07-08 10:29:27 +0200

    tested autoform example content as container

--- src/bda/plone/yafowil_autoform_example/content.py
+++ src/bda/plone/yafowil_autoform_example/content.py
@@ -1,9 +1,11 @@
 from bda.plone.yafowil_autoform_example.interfaces import IYafowilAutoformExample
 from plone.dexterity.content import Item
+from plone.dexterity.content import Container
 from zope.interface import implementer
 
 
 @implementer(IYafowilAutoformExample)
-class YafowilAutoformExample(Item):
+class YafowilAutoformExample(Container):
+# class YafowilAutoformExample(Item):
     """Example Content.
     """
